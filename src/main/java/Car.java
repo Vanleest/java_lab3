d72065b35416811785323ef94db32a28a8d7d67f
Krzysztof Listwan <k.listwan@gmail.com>
2024-11-24 14:13:47 +0100

    poprawka

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,12 +1,16 @@
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@EqualsAndHashCode(of = "producer")
 public class Car {
-    private String model, producer, color;
+    private String model;
+    private String producer;
+    private String color;
     private int year, quantity;
     private double price;
 }
